036b808bfaba90130fbc19d9a007200c60f0bb71
Michael Thomas <michaelthomas0611@gmail.com>
2020-06-05 23:41:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,46 +4,46 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
-
-
-
+  def self.all
+    @@all
+  end
 
   def save
     self.class.all << self
   end
-
+  
   def self.create
     song = self.new
     @@all << song
     song
   end
-
+  
   def self.new_by_name(name)
     song = self.new
     song.name = name
     song
   end
-
+  
   def self.create_by_name(name)
     song = self.new
     song.name = name
     @@all << song
     song
   end
-
+    
   def self.find_by_name(name)
     self.all.find{|person| person.name == name}
   end
-
+  
   def self.find_or_create_by_name(name)
     song_found = find_song = find_by_name(name)
-    if song_found == nil
+    if song_found == nil 
       create_song = create_by_name(name)
     else
       find_song = find_by_name(name)
     end
   end
-
+  
   def self.alphabetical
     puts @@all
     sorted_songs = []
@@ -52,7 +52,7 @@ class Song
     end
     sorted_songs.sort! { |a, b|  a.name <=> b.name }
   end
-
+  
   def self.new_from_filename(filename)
     filename_array = filename.split("-")
     song = self.new
@@ -60,7 +60,7 @@ class Song
     song.artist_name = filename_array[0][0..-2]
     song
   end
-
+  
   def self.create_from_filename(filename)
     filename_array = filename.split("-")
     song = self.new
@@ -69,7 +69,7 @@ class Song
     @@all << song
     song
   end
-
+  
   def self.destroy_all
     @@all.clear
   end
